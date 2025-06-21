e9528c2d6f1eaed170b5838b6a88fad1f4dd7bed
Saran712 <saravananmohan607@gmail.com>
2025-06-21 21:10:48 +0530

    Driver class for Logger

---
+++ week1/design_principles_and_pattern/exercise1/LoggerDriver.java
@@ -0,0 +1,17 @@
+package week1.design_principles_and_pattern.exercise1;
+
+public class LoggerDriver {
+    public static void main(String[] args) {
+        Logger logger1 = Logger.getInstance();
+        logger1.log("First log message");
+
+        Logger logger2 = Logger.getInstance();
+        logger2.log("Second log message");
+
+        if (logger1 == logger2) {
+            System.out.println("Both logger instances are the same.");
+        } else {
+            System.out.println("Different logger instances found!");
+        }
+    }
+}
