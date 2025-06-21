9802d3a5cb000a53b6f4d0763913ea63eb9aed05
Saran712 <saravananmohan607@gmail.com>
2025-06-21 21:58:05 +0530

    Excel Document Factory

---
+++ week1/design_principles_and_pattern/exercise2/ExcelDocumentFactory.java
@@ -0,0 +1,8 @@
+package week1.design_principles_and_pattern.exercise2;
+
+public class ExcelDocumentFactory extends DocumentFactory {
+    @Override
+    public Document createDocument() {
+        return new ExcelDocument();
+    }
+}
