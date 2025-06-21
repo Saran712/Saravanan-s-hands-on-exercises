8f2ba465ab8678395aebfed4dceded6c9f9c6af5
Saravanan Mohan <saravananmohan607@gmail.com>
2025-06-21 23:31:11 +0530

    Delete week1/design_principles_and_pattern/exercise2/ExcelDocument.java

--- week1/design_principles_and_pattern/exercise2/ExcelDocument.java
+++
@@ -1,8 +0,0 @@
-package week1.design_principles_and_pattern.exercise2;
-
-public class ExcelDocument implements Document {
-    @Override
-    public void open() {
-        System.out.println("Opening an Excel document.");
-    }
-}
