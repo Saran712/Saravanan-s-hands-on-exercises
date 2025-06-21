797a18f47ede804c186546519077601d29cc929e
Saravanan Mohan <saravananmohan607@gmail.com>
2025-06-21 23:32:31 +0530

    Delete week1/design_principles_and_pattern/exercise2/WordDocumentFactory.java

--- week1/design_principles_and_pattern/exercise2/WordDocumentFactory.java
+++
@@ -1,8 +0,0 @@
-package week1.design_principles_and_pattern.exercise2;
-
-public class WordDocumentFactory extends DocumentFactory {
-    @Override
-    public Document createDocument() {
-        return new WordDocument();
-    }
-}
