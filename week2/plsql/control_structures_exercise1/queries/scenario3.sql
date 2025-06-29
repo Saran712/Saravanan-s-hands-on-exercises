9f11b5ba1b26ddcdafd54687833640302dab7fb1
Saran712 <saravananmohan607@gmail.com>
2025-06-29 10:55:09 +0530

    scenario3.sql

---
+++ week2/plsql/control_structures_exercise1/queries/scenario3.sql
@@ -0,0 +1,9 @@
+BEGIN
+  FOR rec IN (
+    SELECT customer_id, due_date FROM Loans
+    WHERE due_date <= SYSDATE + 30
+  ) LOOP
+    DBMS_OUTPUT.PUT_LINE('Reminder: Loan for Customer ID ' || rec.customer_id ||
+                         ' is due on ' || TO_CHAR(rec.due_date, 'DD-MON-YYYY'));
+  END LOOP;
+END;
