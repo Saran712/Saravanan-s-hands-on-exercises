a9489ce3f83b5dc4c7290cb7b3243f8de232a9b7
Saran712 <saravananmohan607@gmail.com>
2025-06-29 19:48:44 +0530

    AssertionsTest.java

---
+++ week2/JUnit_Testing/exercise3/AssertionsTest.java
@@ -0,0 +1,24 @@
+package week2.JUnit_Testing.exercise3;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class AssertionsTest {
+
+    @Test
+    public void testAssertions() {
+        
+        assertEquals(5, 2 + 3);
+
+        assertTrue(5 > 3);
+
+        assertFalse(5 < 3);
+
+        assertNull(null);
+
+        assertNotNull(new Object());
+        
+        System.out.println("All the assertions are tested");
+    }
+}
