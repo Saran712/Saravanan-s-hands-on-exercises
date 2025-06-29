31e76406c5d2fd615ef572d1111ba05c22d185cd
Saran712 <saravananmohan607@gmail.com>
2025-06-29 10:55:55 +0530

    scenario2.sql

---
+++ week2/plsql/stored_procedures_exercise3/queries/scenario2.sql
@@ -0,0 +1,9 @@
+CREATE OR REPLACE PROCEDURE UpdateEmployeeBonus(
+  dept_id IN NUMBER,
+  bonus_pct IN NUMBER
+) AS
+BEGIN
+  UPDATE Employees
+  SET salary = salary + (salary * bonus_pct / 100)
+  WHERE department_id = dept_id;
+END;
