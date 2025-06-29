f61c2ef5a99654b27f099e7a7e7a1f90e3e012a9
Saran712 <saravananmohan607@gmail.com>
2025-06-29 17:34:28 +0530

    scenario3.sql

--- week2/plsql/control_structures_exercise1/queries/scenario3.sql
+++ week2/plsql/control_structures_exercise1/queries/scenario3.sql
@@ -1,9 +1,11 @@
 BEGIN
   FOR rec IN (
-    SELECT customer_id, due_date FROM Loans
-    WHERE due_date <= SYSDATE + 30
+    SELECT c.Name, l.EndDate
+    FROM Loans l
+    JOIN Customers c ON l.CustomerID = c.CustomerID
+    WHERE l.EndDate BETWEEN SYSDATE AND SYSDATE + 30
   ) LOOP
-    DBMS_OUTPUT.PUT_LINE('Reminder: Loan for Customer ID ' || rec.customer_id ||
-                         ' is due on ' || TO_CHAR(rec.due_date, 'DD-MON-YYYY'));
+    DBMS_OUTPUT.PUT_LINE('Reminder: Loan for ' || rec.Name || ' is due on ' || TO_CHAR(rec.EndDate, 'YYYY-MM-DD'));
   END LOOP;
 END;
+/
