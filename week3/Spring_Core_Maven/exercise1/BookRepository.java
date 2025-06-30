5493850f88aa3b56587efb61f1faac75f32387a7
Saran712 <saravananmohan607@gmail.com>
2025-06-30 18:17:04 +0530

    Spring core maven

---
+++ week3/Spring_Core_Maven/exercise1/BookRepository.java
@@ -0,0 +1,8 @@
+package com.library.LibraryManagement.repository;
+
+
+public class BookRepository {
+    public String getBookData() {
+        return "Book data from BookRepository";
+    }
+}}
---
+++ week3/Spring_Core_Maven/exercise1/Book_Driver.java
@@ -0,0 +1,14 @@
+package com.library.LibraryManagement;
+
+import com.library.LibraryManagement.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Book_Driver {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        BookService bookService = context.getBean("bookService", BookService.class);
+        bookService.displayBook();
+    }
+}
