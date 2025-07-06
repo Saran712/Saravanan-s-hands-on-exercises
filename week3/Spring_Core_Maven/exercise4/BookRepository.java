a6fd212f35fbef184fb9ee4797bca6ebacb4c980
Saravanan Mohan <saravananmohan607@gmail.com>
2025-07-06 06:53:21 +0530

    Mandatory hands-on exercises

--- week3/Spring_Core_Maven/exercise4/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.library.LibraryManagements.repository;
-
-public class BookRepository {
-    public String getBookData() {
-        return "Spring Dependencies via Maven loaded successfully!";
-    }
-}
