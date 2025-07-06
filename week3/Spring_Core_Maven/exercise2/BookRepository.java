ed13ce3ad485b30812edd93b9c9d96a4c412c523
Saravanan Mohan <saravananmohan607@gmail.com>
2025-07-06 06:53:04 +0530

    Mandatory hands-on exercises

--- week3/Spring_Core_Maven/exercise2/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.library.LibraryManagement.repository;
-
-public class BookRepository {
-    public String getBookData() {
-        return "Dependency Injection Successful: BookRepository is injected into BookService.";
-    }
-}
